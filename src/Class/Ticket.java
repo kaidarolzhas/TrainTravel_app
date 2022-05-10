3d5263e5630b97d9dc0de11660269f06e8d53c2a
Tan3103 <tannur.uataev@mail.ru>
2022-05-11 00:10:23 +0600

    z

---
+++ src/Class/Ticket.java
@@ -0,0 +1,14 @@
+package Class;
+
+public class Ticket {
+    private Integer id;
+    private String whereFrom;
+    private String where;
+    private int price;
+    private String date;
+
+    public Ticket() {
+    }
+
+
+}
