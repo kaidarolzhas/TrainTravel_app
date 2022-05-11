5e40542f61736adfc10121cffd3792783877c4eb
Tan3103 <tannur.uataev@mail.ru>
2022-05-11 19:43:19 +0600

    add user menu

--- src/Main/MainFrame.java
+++ src/Main/MainFrame.java
@@ -1,9 +1,14 @@
 package Main;
 
+import UserMenu.BuyTicket;
+import UserMenu.UserMenu;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
     public static MainMenu mainMenu;
+    public static BuyTicket buyTicket;
+    public static UserMenu userMenu;
     public MainFrame(){
         setSize(600, 400);
         setTitle("Train Travel");
@@ -12,8 +17,17 @@ public class MainFrame extends JFrame {
 
         mainMenu = new MainMenu();
         mainMenu.setLocation(0, 0);
+        mainMenu.setVisible(true);
         add(mainMenu);
 
+        buyTicket = new BuyTicket();
+        buyTicket.setLocation(0, 0);
+        buyTicket.setVisible(false);
+        add(buyTicket);
 
+        userMenu = new UserMenu();
+        userMenu.setLocation(0, 0);
+        userMenu.setVisible(false);
+        add(userMenu);
     }
 }
--- src/Main/MainMenu.java
+++ src/Main/MainMenu.java
@@ -2,10 +2,11 @@ package Main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends Container {
     public MainMenu(){
-
         setSize(600, 400);
         setLayout(null);
 
@@ -14,14 +15,12 @@ public class MainMenu extends Container {
         welcome.setFont(new Font("Verdana", Font.BOLD, 20));
         add(welcome);
 
-
         JLabel tip = new JLabel("LOGIN OR SIGN UP TO CONTINUE");
         tip.setVerticalAlignment(JLabel.CENTER);
         welcome.setBounds(100, 40, 300, 30);
         welcome.setFont(new Font("Verdana", Font.BOLD, 14));
         tip.setHorizontalAlignment(JLabel.CENTER);
         add(tip);
-
 
         JButton logInButton = new JButton("Log in");
         logInButton.setFont(new Font("Verdana", Font.BOLD , 12));
@@ -38,8 +37,25 @@ public class MainMenu extends Container {
         exitButton.setBounds(375, 150, 145, 45);
         add(exitButton);
 
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
 
+        singButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
