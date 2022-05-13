3750c5f309b85c165c216d177f2a79a0681d7d40
Tan3103 <tannur.uataev@mail.ru>
2022-05-14 01:41:38 +0600

    fix error

--- src/Frames/Login.java
+++ src/Frames/Login.java
@@ -1,7 +1,5 @@
 package Frames;
-
 import javax.swing.*;
-
 import Database.DBManager;
 import Database.Package;
 import Main.Main;
@@ -11,16 +9,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 import static Database.DBManager.connection;
 
 public class Login extends Container {
     public static Customer customer;
     public Login(){
-
         setSize(600,400);
         setLayout(null);
-
 
         JLabel loginLabel = new JLabel("LOGIN:");
         loginLabel.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -31,31 +26,24 @@ public class Login extends Container {
         loginField.setBounds(225, 100, 200, 30);
         add(loginField);
 
-
-
         JLabel passwordLabel = new JLabel("PASSWORD:");
         passwordLabel.setFont(new Font("Serif", Font.PLAIN, 14));
         passwordLabel.setBounds(125, 150, 100, 30);
         add(passwordLabel);
 
-
         JPasswordField passwordField = new JPasswordField();
         passwordField.setBounds(225, 150, 200, 30);
         add(passwordField);
-
-
 
         JButton logInButton = new JButton("LOG IN");
         logInButton.setFont(new Font("Verdana", Font.BOLD , 12));
         logInButton.setBounds(125, 200, 145, 30);
         add(logInButton);
 
-
         JButton backButton = new JButton("BACK");
         backButton.setFont(new Font("Verdana", Font.BOLD , 12));
         backButton.setBounds(275, 200, 145, 30);
         add(backButton);
-
 
         logInButton.addActionListener(new ActionListener() {
             @Override
@@ -71,25 +59,25 @@ public class Login extends Container {
                     Main.connect(pd);
 
                     if(resultSet.next()){
+                        loginField.setText(null);
+                        passwordField.setText(null);
+
                         Main.frame.login.setVisible(false);
                         Main.frame.userMenu.setVisible(true);
                         JOptionPane.showInternalMessageDialog(null,"WELCOME");
+                    }else if(loginField.getText().equals("admin") && passwordField.getText().equals("password")){
+                        loginField.setText(null);
+                        passwordField.setText(null);
 
-
-                    }
-
-                    else if(loginField.getText().equals("admin") && passwordField.getText().equals("password")){
                         Main.frame.login.setVisible(false);
                         Main.frame.adminMenu.setVisible(true);
-
-
+                    }else{
+                        JOptionPane.showInternalMessageDialog(null,"Incorrect login or password");
                     }
 
                 }catch (Exception a){
                     a.printStackTrace();
                 }
-
-
             }
         });
 
@@ -100,7 +88,5 @@ public class Login extends Container {
                 Main.frame.mainMenu.setVisible(true);
             }
         });
-
-
     }
 }
